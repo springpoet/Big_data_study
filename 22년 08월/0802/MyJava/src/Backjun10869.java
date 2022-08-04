3f45768558fbc7173b3840d52751e704b1dd7c86
springpoet <ksksksksksmss@gmail.com>
2022-08-04 17:47:16 +0900

    220804 보충 끝

---
+++ 22년 08월/0802/MyJava/src/Backjun10869.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Backjun10869 {
+
+	public static void Main(String[] args) {
+		Scanner n=new Scanner(System.in);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
