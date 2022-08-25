6f91fc0b95cbcee47015540fe5513c089888b4a3
springpoet <ksksksksksmss@gmail.com>
2022-08-25 17:34:32 +0900

    220825 보충 edit update

--- 백준/Java/연습/BaekjoonPractice/src/Baekjoon1009.java
+++ 백준/Java/연습/BaekjoonPractice/src/Baekjoon1009.java
@@ -1,4 +1,10 @@
-
+import java.util.*;
 public class Baekjoon1009 {
-
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		int t=scan.nextInt();
+		for(int i=0;i<t;i++) {
+			
+		}
+	}
 }
