1cf059f4e55940c919cbffcf4a7deb7beb6e3fbe
springpoet <ksksksksksmss@gmail.com>
2022-10-24 16:41:45 +0900

    2201024 class update

---
+++ 22년 10월/1021(JSP)/HelloJSP12_mvcBoard/src/com/javalec/ex/dao/BDao.java
@@ -0,0 +1,143 @@
+package com.javalec.ex.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.javalec.ex.dto.BDto;
+
+public class BDao {
+
+	DataSource ds;
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	//context.xml의 Resource 활용한 것(=커넥션 풀 활용)
+	public BDao() {
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource)ctx.lookup
+					("java:comp/env/jdbc/mysql");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<BDto> list() {
+		ArrayList<BDto> dtos = new ArrayList<BDto>();
+		try {
+			String sql = "select bId, bName, bTitle,"
+					+ "bContent, bDate, bHit,"
+					+ "bGroup, bStep, bIndent from "
+					+ "mvc_board order by bGroup desc,"
+					+ "bStep asc";
+			System.out.println(sql);
+			conn=ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				int bId = rs.getInt("bId");
+				String bName = rs.getString("bName");
+				String bTitle = rs.getString("bTitle");
+				String bContent = rs.getString("bContent");
+				Timestamp bDate = rs.getTimestamp("bDate");
+				int bHit = rs.getInt("bHit");
+				int bGroup = rs.getInt("bGroup");
+				int bStep = rs.getInt("bStep");
+				int bIndent = rs.getInt("bIndent");
+				
+				BDto dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
+				dtos.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return dtos;
+	}
+	
+	public BDto contentView(String strID) {
+		upHit(strID);
+		BDto dto=null;
+		try {
+			conn=ds.getConnection();
+			String sql="select * from mvc_board where bId=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, Integer.parseInt(strID));
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				int bId = rs.getInt("bId");
+				String bName = rs.getString("bName");
+				String bTitle = rs.getString("bTitle");
+				String bContent = rs.getString("bContent");
+				Timestamp bDate = rs.getTimestamp("bDate");
+				int bHit = rs.getInt("bHit");
+				int bGroup = rs.getInt("bGroup");
+				int bStep = rs.getInt("bStep");
+				int bIndent = rs.getInt("bIndent");
+				
+				dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally {
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+				// TODO: handle exception
+			}
+		}
+		return dto;
+	}
+	
+	private void upHit(String strID) {
+		try {
+			conn=ds.getConnection();
+			String sql="update mvc_board set bHit=bHit+1 where bId=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, strID);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
