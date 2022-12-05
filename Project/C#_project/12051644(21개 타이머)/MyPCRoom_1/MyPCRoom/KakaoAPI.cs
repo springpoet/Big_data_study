16ef5f310a1c0a96652a08919e29bd2a1f01eaa7
springpoet <ksksksksksmss@gmail.com>
2022-12-05 16:46:37 +0900

    221205 project update
    타이머 추가

---
+++ Project/C#_project/12051644(21개 타이머)/MyPCRoom_1/MyPCRoom/KakaoAPI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace MyPCRoom
+{
+    public class KakaoAPI
+    {
+        // 클래스 메소드 만듦
+        // KakaoAPI.Search("대구");
+
+        public static List<Locale> Search(string text)
+        {
+            List<Locale> list = new List<Locale>();
+
+            string site = "https://dapi.kakao.com/v2/local/search/keyword.json";
+            string query = $"{site}?query={text}";
+            string restApiKey = "894d02d154f5deca7c46014d9247a0ad";
+            string Header = $"KakaoAK {restApiKey}";
+
+            //Using System.Net
+            WebRequest request = WebRequest.Create(query);
+            request.Headers.Add("Authorization", Header);
+
+            // 응답받기
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            string json = reader.ReadToEnd();
+
+            // alt + enter -> using System.Web.Script.Serialization() 선택
+            JavaScriptSerializer js = new JavaScriptSerializer();
+
+            dynamic dob = js.Deserialize<dynamic>(json);
+            dynamic docs = dob["documents"];
+            object[] buf = docs;
+            int length = buf.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                string lname = docs[i]["place_name"];
+                double x = double.Parse(docs[i]["x"]);
+                double y = double.Parse(docs[i]["y"]);
+                list.Add(new Locale(lname, y, x));
+            }
+            
+
+            return list;
+        }
+
+    }
+}
---
+++ Project/C#_project/김성근 작업/MyPCRoom/Form5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPCRoom
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+            // button을 누르지 않고 enter치면 바로 검색됨
+            textBox1.KeyUp += (sender, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                    button1.PerformClick();
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Locale> locales = KakaoAPI.Search(textBox1.Text);
+            listBox1.Items.Clear();
+            foreach (Locale item in locales)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+
+            Locale ml = listBox1.SelectedItem as Locale; //선택한 것을 Locale로 변환
+            object[] pos = new object[] { ml.Lat, ml.Lng };
+            HtmlDocument hdoc = webBrowser1.Document;
+            // webBrowser 안에 setCenter를 호출한 것
+            // pos가 매개변수가 된 것
+            hdoc.InvokeScript("setCenter", pos);
+           
+        }
+    }
+}
